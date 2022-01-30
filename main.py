bc5c016d3f57738efc03e241849892dea90008d2
Hausdorff94 <jhonyal58@gmail.com>
2022-01-30 14:18:23 -0500

    Class user models

--- main.py
+++ main.py
@@ -1,6 +1,56 @@
+# python
+
+from uuid import UUID
+from datetime import date
+from typing import Optional
+
+# pydantic
+
+from pydantic import BaseModel
+from pydantic import EmailStr
+from pydantic import Field
+
+# FastAPI
 from fastapi import FastAPI
 
 app = FastAPI()
+
+# Models
+
+class UserBase(BaseModel):
+    user_id: UUID = Field(
+        ..., 
+        alias="id",
+        title="User ID",
+        )
+    email: EmailStr = Field(
+        ..., 
+        alias="email"
+        )
+
+class UserLogin(UserBase):
+    password: str = Field(
+        ...,
+        min_length=8,
+    )
+
+class User(UserBase):
+    first_name: str = Field(
+        ...,
+        min_length=2,
+        max_length=50,
+    )
+    last_name: str = Field(
+        ...,
+        min_length=2,
+        max_length=50,
+    )
+    birth_date: Optional[date] = Field(default=None)
+
+
+class Tweet(BaseModel):
+    pass
+
 
 @app.get(
     path="/"
